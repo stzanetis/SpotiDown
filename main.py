2615dba5ef5cb393e153383e2c51e9bbaf67e9bb
Savvas Tzaneths <94377331+savvastzan@users.noreply.github.com>
2023-12-15 11:59:52 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,64 +2,41 @@ import csv
 import os
 import re
 
+import requests as rq
 import spotipy
 from pytube import YouTube
 from pytube import Search
 from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+#from moviepy.editor import *
 
-# load credentials from .env file
-load_dotenv()
-CLIENT_ID = os.getenv("CLIENT_ID", "")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
-OUTPUT_FILE_NAME = "track_info.csv"
+def printPlaylists(playlists):
+    print("Loading playlists..")
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s" % (i+1, playlist['name']))
 
-# authenticate and create sp spotify session object
-client_credentials_manager = SpotifyClientCredentials(
-    client_id=CLIENT_ID, client_secret=CLIENT_SECRET
-)
-auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(auth_manager=auth_manager)
+        user_input = input("Enter the number of the playlist you want to select: ")
+        try:
+            selected_index = int(user_input) - 1  # Adjust index to match 0-based indexing
+            selected_playlist = playlists['items'][selected_index]['name']
+            print("You selected playlist:", selected_playlist)
+            playlist_uri = playlists['items'][selected_index]['uri']
+            return playlist_uri
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter a valid playlist number.")
 
-try:
-    username = input("Spotify account username: ")
-except (ValueError, IndexError):
-    print("Invalid username.")
-    exit()
+        if playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
 
-playlists = sp.user_playlists(username)
-print("Loading playlists..")
-while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        print("%4d %s" % (i+1, playlist['name']))
+def downloadSongs(tracks):
+    # create output path
+    #output_folder = "C:" + os.getcwd() + "\Output\\"
+    #os.mkdir(output_folder)
 
-    user_input = input("Enter the number of the playlist you want to select: ")
-    try:
-        selected_index = int(user_input) - 1  # Adjust index to match 0-based indexing
-        selected_playlist = playlists['items'][selected_index]['name']
-        print("You selected playlist:", selected_playlist)
-        playlist_uri = playlists['items'][selected_index]['uri']
-    except (ValueError, IndexError):
-        print("Invalid input. Please enter a valid playlist number.")
-
-    if playlists['next']:
-        playlists = sp.next(playlists)
-    else:
-        playlists = None
-
-# get list of tracks in a given playlist (note: max playlist length 100)
-tracks = sp.playlist_tracks(playlist_uri)["items"]
-a = len(tracks)
-print(a)
-
-# create csv file
-with open(OUTPUT_FILE_NAME, "w", encoding="utf-8") as file:
-    writer = csv.writer(file)
-    
-    # write header column names
-    writer.writerow(["track", "artist"])
-
-    # extract name and artist
     for track in tracks:
         name = track["track"]["name"]
         artists = ", ".join(
@@ -69,7 +46,47 @@ with open(OUTPUT_FILE_NAME, "w", encoding="utf-8") as file:
         s = Search(artist_title)
         yt = s.results[0]
         stream = yt.streams.filter(only_audio=True).first()
-        stream.download()
+        stream.download() #add output folder in other cases except KHD
+        #video = VideoFileClip("example.mp4")
+        #video.audio.write_audiofile("example.mp3")
 
-        # write to csv
-        writer.writerow([name, artists])
+# load credentials from .env file
+load_dotenv()
+CLIENT_ID = os.getenv("CLIENT_ID", "")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
+SPOTIPY_REDIRECT_URI="http://localhost:8888/callback"
+
+# ask user for type of login
+cho1 = input("1.Find Playlists by username    2.Login to your account    3.Get liked songs(only from login)    4.Quit\nSelect an option: ")
+if cho1 == '1':
+    # authenticate and create sp spotify session object
+    client_credentials_manager = SpotifyClientCredentials(
+        client_id=CLIENT_ID, client_secret=CLIENT_SECRET
+    )
+    auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    sp = spotipy.Spotify(auth_manager=auth_manager)
+
+    # get account username from user
+    try:
+        username = input("Spotify account username: ")
+    except (ValueError, IndexError):
+        print("Invalid username.")
+        exit()
+
+    playlists = sp.user_playlists(username)
+    playlist_uri = printPlaylists(playlists)
+    tracks = sp.playlist_tracks(playlist_uri)["items"]
+    downloadSongs(tracks)
+elif cho1 == '2':
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope="playlist-read-private"))
+    playlists = sp.current_user_playlists()
+    playlist_uri = printPlaylists(playlists)
+    tracks = sp.playlist_tracks(playlist_uri)["items"]
+    downloadSongs(tracks)
+elif cho1 == '3':
+    #not working yet, forbidden error 403
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI))
+    tracks = sp.current_user_saved_tracks(market="GR")
+    downloadSongs(tracks)
+elif cho1 == '4':
+    exit()
