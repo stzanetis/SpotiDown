1fa3f7fd5edafe20c5e8644e0ce05ddb5500e872
savvastzanetis@gmail.com <Savvas Tzanetis>
2024-11-07 15:16:21 +0200

    Changed download library from 'pytube' to 'yt-dlp'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SpotiDown
-SpotiDown is a python based program that downloads your spotify music playlists even without a premium subscription from youtube. You can download any public playlist with just the username of the creator, as well as any of your playlists and liked songs by granting access to the app.
+SpotiDown is a python based program that downloads your spotify music playlists even without a premium subscription, from youtube. All you need to do is login with your personal Spotify account and you can choose to download any of your playlists or favorite songs! 
 
 *Right now you can only use this by creating your own app in spotify web developer and putting your client id and secret to a .env file
--- main.py
+++ main.py
@@ -1,130 +1,115 @@
-import os
-import re
-import spotipy
-from pytube import YouTube
-from dotenv import load_dotenv
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-from youtubesearchpython import VideosSearch
-from moviepy.editor import VideoFileClip
-# import music-tag
+import os, dotenv, spotipy, wget
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+from ytmusicapi import YTMusic
+from yt_dlp import YoutubeDL
 
-# Libraries that may not be needed
-import requests as rq
+# Print users playlists and return the songs from the selected playlist
+def selectPlaylist(playlists):
+    print("   0) Liked Songs")
+    for i, playlist in enumerate(playlists['items']):
+            print("%4d) %s" % (i+1, playlist['name']))
 
-def printPlaylists(playlists):
-    print("Loading playlists..")
-    while playlists:
-        for i, playlist in enumerate(playlists['items']):
-            print("%4d %s" % (i+1, playlist['name']))
+    while playlists:        
+        userinput = input("Enter the number of the playlist you want to select: ")
+        if userinput  == "0":
+            all_tracks = []
+            offset = 0
+            while True:
+                # Fetch 50 songs at a time
+                batch = sp.current_user_saved_tracks(limit=50, offset=offset)
+                items = batch['items']
+                all_tracks.extend(items)
+                # Stop when there are no more songs to fetch
+                if len(items) < 50:
+                    break
+                offset = offset + 50
+            return all_tracks
+        else:
+            try:
+                selected_index = int(userinput) - 1  # Adjust index to match 0-based indexing
+                selected_playlist = playlists['items'][selected_index]['name']
+                print("You selected playlist:", selected_playlist)
+                playlist_uri = playlists['items'][selected_index]['uri']
+                return sp.playlist_tracks(playlist_uri)["items"]
+            except (ValueError, IndexError):
+                print("Invalid input. Please enter a valid playlist number.")
 
-        user_input = input("Enter the number of the playlist you want to select: ")
-        try:
-            selected_index = int(user_input) - 1  # Adjust index to match 0-based indexing
-            selected_playlist = playlists['items'][selected_index]['name']
-            print("You selected playlist:", selected_playlist)
-            playlist_uri = playlists['items'][selected_index]['uri']
-            return playlist_uri
-        except (ValueError, IndexError):
-            print("Invalid input. Please enter a valid playlist number.")
-
+        """
+        #I don't know what this is..
         if playlists['next']:
             playlists = sp.next(playlists)
         else:
             playlists = None
+        """
 
-def searchSongs(tracks, cho):
-    if cho == '3':
-        for track in tracks["items"]:
-            name = track["track"]["name"]
-            artist = track["track"]["artists"][0]["name"]
-            artist_title = artist + " " + name
-            videosSearch = VideosSearch(artist_title, limit = 1)
-            if videosSearch.result():
-                # Access the first result and extract the video URL
-                video_url = videosSearch.result()['result'][0]['link']
-                yttitle = downloadSongs(video_url)
-                mp4Tomp3(artist_title, yttitle)
-            else:
-                print("No results found.")
-    else:
-        for track in tracks:
-            name = track["track"]["name"]
-            artist = track["track"]["artists"][0]["name"]
-            artist_title = artist + " " + name
-            videosSearch = VideosSearch(artist_title, limit = 1)
-            if videosSearch.result():
-                # Access the first result and extract the video URL
-                video_url = videosSearch.result()['result'][0]['link']
-                yttitle = downloadSongs(video_url)
-                mp4Tomp3(artist_title, yttitle)
-            else:
-                print("No results found.")
+# Search Youtube for the selected songs and download them
+def downloadTracks(tracks):
+    print("Starting to download %d song(s).." % (len(tracks)))
+    error_counter = 0
+    for track in tracks:
+        # Extract song URLs from Youtube Music
+        searchtitle = track["track"]["name"] + " " + track["track"]["artists"][0]["name"] + " (Audio)"
+        search_results = YTMusic().search(searchtitle)[:1] # [:1] is a limiter to limit results to 1 item only, filter="songs" to return only actual songs from Youtube Music
+        video_ids = [item["videoId"] for item in search_results if "videoId" in item]
+        if video_ids == []:
+            error_counter = error_counter + 1
+            continue
+        if video_ids[0] == None:
+            search_results = YTMusic().search(searchtitle, filter="songs")[:1]
+            video_ids = [item["videoId"] for item in search_results if "videoId" in item]
+        songurl = "https://www.youtube.com/watch?v=" + video_ids[0]
 
-def downloadSongs(url):
-    # Create output path
-    output_folder = os.getcwd() + "\\Output"
-    if not (os.path.exists(output_folder) and os.path.isdir(output_folder)):
-        os.mkdir(output_folder)
+        # Create output path
+        output_folder = os.getcwd() + "\\bin"
+        if not (os.path.exists(output_folder) and os.path.isdir(output_folder)):
+            os.mkdir(output_folder)
+        
+        # Downloader options
+        ydl_opts = {
+            'ffmpeg_location': os.getcwd() + "\\ffmpeg\\bin\\ffmpeg.exe",
+            'format': 'bestaudio',
+            'outtmpl': output_folder + "\\%(artist)s - %(title)s.%(ext)s",
+            'noplaylist': True,
+            'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192'
+                }],
+            'quiet': True,
+            'sponsorblock-remove': ['all']
+        }
+        try:
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download(songurl)
+        except Exception:
+            error_counter = error_counter + 1
+        #insertMetadata(track)
+    print("Downloading Proccess Complete. %d song(s) failed to download." % (error_counter))
+
+# Add metadata to the downloaded songs
+def insertMetadata(track):
+    print("Downloading and adding metadata for each song..")
+    img = track["track"]["album"]["images"][0]["url"]
+    name = track["track"]["name"]
+    artist_name = track["track"]["artists"][0]["name"]
+    album_name = track["track"]["album"]["name"]
+    release_date = track["track"]["album"]["release_date"]
+    print("name: %s, img: %s, artist_name: %s, album_name: %s, release_date: %s" % (name, img, artist_name, album_name, release_date))
+    #wget.download(img)
     
-    # Download songs with pytube
-    yt = YouTube(url)
-    stream = yt.streams.filter(file_extension='mp4').first()
-    stream.download(output_folder)
-    return yt.title # Return title for converting the mp4 file to an mp3
 
-def mp4Tomp3(songname, yttitle):
-    # Define the output path
-    output_folder = os.getcwd() + "\\Output"
 
-    # Modified path with removed special characters that got here from the song names
-    sanitized_path = re.sub(r'[^\w\s\(\)\&\-\\-_]', '', output_folder + "\\" + yttitle)
-
-    video = VideoFileClip(sanitized_path + ".mp4")
-    video.audio.write_audiofile(output_folder + "\\" + songname + ".mp3")
-
-    #os.remove(output_folder + "\\" + yt.title + ".mp4")
-
-# Load api credentials from .env file
-load_dotenv()
+# Load Spotify API credentials from .env file
+dotenv.load_dotenv()
 CLIENT_ID = os.getenv("CLIENT_ID", "")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
 REDIRECT_URI="http://localhost:8888/callback"
 
-# Ask user for type of login
-cho1 = input("1.Find Playlists by username    2.Login to your account    3.Get liked songs(only from login)\nSelect an option: ")
-if cho1 == '1':
-    # Authenticate and create sp spotify session object
-    client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    auth_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    sp = spotipy.Spotify(auth_manager=auth_manager)
+# Authenticate and create sp spotify session object
+print("Login to your Spotify account through your browser to continue..")
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="user-library-read"))
+print("Succesful Login!")
 
-    try: # Get account username from user
-        username = input("Spotify account username: ")
-    except (ValueError, IndexError):
-        print("Invalid username.")
-        exit()
-
-    # Extract the public playlists from spotify    
-    playlists = sp.user_playlists(username)
-    playlist_uri = printPlaylists(playlists)
-    tracks = sp.playlist_tracks(playlist_uri)["items"]
-
-    # Search and download songs
-    searchSongs(tracks, cho1)
-elif cho1 == '2':
-    # Extract the playlists from spotify after succesful login from user
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="playlist-read-private"))
-    playlists = sp.current_user_playlists()
-    playlist_uri = printPlaylists(playlists)
-    tracks = sp.playlist_tracks(playlist_uri)["items"]
-
-    # Search and download songs
-    searchSongs(tracks, cho1)
-elif cho1 == '3':
-    # Extract the liked songs from spotify after succesful login from user
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="user-library-read"))
-    tracks = sp.current_user_saved_tracks(limit=10)
-
-    # Download the songs
-    searchSongs(tracks, cho1)
+tracks = selectPlaylist(sp.current_user_playlists())
+downloadTracks(tracks)
