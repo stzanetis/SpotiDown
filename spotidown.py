42782b537300cfc6c018781b6b4ca078bee1c750
Savvas Tzanetis <savvastzanetis@gmail.com>
2024-12-21 19:33:10 +0200

    Added better looking CLI using the rich library

--- spotidown.py
+++ spotidown.py
@@ -1,16 +1,45 @@
-import os, dotenv, spotipy, wget, music_tag                         # type: ignore
+import os, dotenv, spotipy, wget, music_tag, signal, time           # type: ignore
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth   # type: ignore
 from ytmusicapi import YTMusic                                      # type: ignore
 from yt_dlp import YoutubeDL                                        # type: ignore
 
+# Rich library imports for console output
+from rich.console import Console                                    # type: ignore
+from rich.columns import Columns                                    # type: ignore
+from rich.prompt import Prompt                                      # type: ignore
+from rich.progress import Progress                                  # type: ignore
+
+logo = """                                                                                     
+   _ _ _     _                      _          _____         _   _ ____                
+  | | | |___| |___ ___ _____ ___   | |_ ___   |   __|___ ___| |_|_|    \ ___ _ _ _ ___ 
+  | | | | -_| |  _| . |     | -_|  |  _| . |  |__   | . | . |  _| |  |  | . | | | |   |
+  |_____|___|_|___|___|_|_|_|___|  |_| |___|  |_____|  _|___|_| |_|____/|___|_____|_|_|
+                                                    |_|                                                                                                        
+"""
+
+# Custom logger class for YoutubeDL
+class Logger:
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
 # Print users playlists and return the songs from the selected playlist
 def selectPlaylist(playlists):
-    print("   0) Liked Songs")
-    for i, playlist in enumerate(playlists['items']):
-            print("%4d) %s" % (i+1, playlist['name']))
+    # Display all availabel options
+    playlist_names = ["Liked Songs"] + [playlist['name'] for playlist in playlists['items']]                    # Add "Liked Songs" as the first item
+    numbered_playlists = [f"[bold blue]{i}.[/bold blue] {name}" for i, name in enumerate(playlist_names)]       # Create a list of numbered playlists
+    formatted_columns = ["\n".join(numbered_playlists[i:i + 4]) for i in range(0, len(numbered_playlists), 4)]  # Format into 3 items per column
 
-    while playlists:        
-        userinput = input("Enter the number of the playlist you want to select: ")
+    console.print("[bold green]Available Playlists:[/bold green]")
+    console.print(Columns(formatted_columns, expand=True))
+
+    while playlists:
+        userinput = Prompt.ask("Enter the number of the playlist you want to select")     
         if userinput  == "0":
             all_tracks = []
             offset = 0
@@ -23,21 +52,21 @@ def selectPlaylist(playlists):
                 if len(items) < 50:
                     break
                 offset = offset + 50
-            return all_tracks
+                return all_tracks
         else:
             try:
                 selected_index = int(userinput) - 1  # Adjust index to match 0-based indexing
                 selected_playlist = playlists['items'][selected_index]['name']
-                print("You selected playlist:", selected_playlist)
+                console.print(f"You selected playlist: [bold green]{selected_playlist}[/bold green]")
                 playlist_uri = playlists['items'][selected_index]['uri']
                 return sp.playlist_tracks(playlist_uri)["items"]
             except (ValueError, IndexError):
-                print("Invalid input. Please enter a valid playlist number.")
+                console.print("[bold red]Invalid input[/bold red]. Please enter a [bold green]valid[/bold green] playlist number.")
 
 # Search Youtube for the selected songs and download them
 def downloadTracks(tracks):
     print("")
-    print("Starting to download %d song(s).." % (len(tracks)))
+    console.print(f"Starting to download [bold blue]{len(tracks)}[/bold blue] [bold purple]songs[/bold purple]...")
     error_counter = 0
 
     # Create output folder if it doesn't exist
@@ -47,7 +76,6 @@ def downloadTracks(tracks):
 
     for track in tracks:
         # Extract song URLs from Youtube Music
-        print("Downloading: %s" % (track["track"]["name"]))
         searchtitle = track["track"]["name"] + " " + track["track"]["artists"][0]["name"] + " (Audio)"
 
         search_results_songs = YTMusic().search(searchtitle, filter="songs")[:4]    # [:10] is a limiter to limit results to 10 items, 
@@ -78,21 +106,23 @@ def downloadTracks(tracks):
                     'preferredquality': '192'
                 }],
             'quiet': True,
-            'sponsorblock-remove': ['all']
+            'logger': Logger(),
+            'sponsorblock-remove': ['all'],
         }
         try:
+            #print("Downloading: %s" % (track["track"]["name"]))
             with YoutubeDL(ydl_opts) as ydl:
                 ydl.download(songurl)
                 info_dict = ydl.extract_info(songurl, download=False)
                 filename = ydl.prepare_filename(info_dict)
                 if filename.endswith(".webm"):
                     filename = filename.replace(".webm", "")
-
                 insertMetadata(track, filename)
         except Exception:
             error_counter = error_counter + 1
-    
-    print("Downloading Proccess Complete. %d song(s) failed to download." % (error_counter))
+
+    console.print()
+    return error_counter
 
 # Add metadata to the downloaded songs
 def insertMetadata(track, mp3name):
@@ -102,7 +132,7 @@ def insertMetadata(track, mp3name):
         os.mkdir(temp_folder)
     
     # Download necessary metadata
-    print("Downloading and adding metadata to: %s" % (track["track"]["name"]))
+    #print("Downloading and adding metadata to: %s" % (track["track"]["name"]))
     img = track["track"]["album"]["images"][0]["url"]
     name = track["track"]["name"]
     artist_name = track["track"]["artists"][0]["name"]
@@ -133,7 +163,7 @@ def insertMetadata(track, mp3name):
 
     # Replace original file with the new one
     os.replace(f"{mp3_path}.temp", mp3_path)
-    print(f"Metadata added and saved to {mp3_path}")
+    #print(f"Metadata added and saved to {mp3_path}")
 
 # Find the best matching song from the search results
 def findBestSong(track, search_results):
@@ -145,8 +175,8 @@ def findBestSong(track, search_results):
     spotify_artist = track["track"]["artists"][0]["name"].lower()
     spotify_duration = track["track"]["duration_ms"] / 1000
     for result in search_results:
-        print("Spotify name: %s, Spotify artist: %s, Spotify duration: %d" % (spotify_name, spotify_artist, spotify_duration))
-        print("Youtube name: %s, Youtube artist: %s, Youtube duration: %d" % (result["title"].lower(), result["artists"][0]["name"].lower(), result["duration_seconds"]))
+        #print("Spotify name: %s, Spotify artist: %s, Spotify duration: %d" % (spotify_name, spotify_artist, spotify_duration))
+        #print("Youtube name: %s, Youtube artist: %s, Youtube duration: %d" % (result["title"].lower(), result["artists"][0]["name"].lower(), result["duration_seconds"]))
         youtube_name = result["title"].lower()
         youtube_artist = result["artists"][0]["name"].lower()
         youtube_duration = result["duration_seconds"]
@@ -157,11 +187,21 @@ def findBestSong(track, search_results):
         score = (w_artist * artist_match +
                  w_name * name_match +
                  w_duration * duration_match)
-        print("Score: %f" % (score))
+        #print("Score: %f" % (score))
         if score > best_score:
             best_score = score
             best_match = result
     return best_match
+
+# Signal handler for keyboard interrupt
+def signal_handler(sig, frame):
+    console.print("\n[bold red]Process terminated by user. Exiting...[/bold red]")
+    exit(0)
+
+# Main program
+signal.signal(signal.SIGINT, signal_handler)
+console = Console()
+console.print(logo, style="bold green")
 
 # Load Spotify API credentials from .env file
 dotenv.load_dotenv()
@@ -170,13 +210,16 @@ CLIENT_SECRET = os.getenv("CLIENT_SECRET", "")
 REDIRECT_URI="http://localhost:8888/callback"
 
 # Authenticate and create sp spotify session object
-print("Login to your Spotify account through your browser to continue..")
+console.print("Login to your [bold green]Spotify[/bold green] account through your browser to continue: ", end="")
 client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="user-library-read"))
-print("Succesful Login!")
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope="user-library-read playlist-read-private"))
+console.print("[bold blue]Succesful Login![/bold blue]")
 
+# Select playlist and download songs
 tracks = selectPlaylist(sp.current_user_playlists())
-downloadTracks(tracks)
+errors = downloadTracks(tracks)
 
 print("")
-print("All done! Enjoy your music!")
+if errors > 0:
+    console.print(f"[bold red]{errors} songs could not be downloaded.[/bold red]")
+console.print("[bold green]All done! Enjoy your music![/bold green]")
